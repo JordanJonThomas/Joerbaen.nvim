f2ec65e49c4b95b30882254c470aabfd0388be29
Jordan Thomas <jordanjothomas@gmail.com>
2026-01-02 17:06:47 -0500

    Refactor lsp setup
    - Updated blink setup
    - Now using core gd instead of telescope.builtin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,24 @@
 return {
-    {
-        'mason-org/mason.nvim',
-        opts={},
-    },
+    { 'mason-org/mason.nvim', opts={} },
 
     {
-        'mason-org/mason-lspconfig.nvim', 
-        dependencies = {'neovim/nvim-lspconfig' },
-        config = function() 
+        'mason-org/mason-lspconfig.nvim',
+        dependencies = { 'neovim/nvim-lspconfig', 'saghen/blink.cmp' },
+        config = function()
+            local caps = require('blink.cmp').get_lsp_capabilities()
+            vim.lsp.config('*', {
+                capabilities = caps,
+            })
+
+            -- lsp servers to install automatically
             vim.lsp.enable({
                 'rust_analyzer',
                 'lua_ls',
                 'ts_ls'
             })
 
-            local builtin = require('telescope.builtin') -- telescopalicious
-
             -- map keys on lsp attach
+            local builtin = require('telescope.builtin') -- telescopalicious
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('jt-on-lsp-attach', {clear = true}),
                 callback = function(event)
@@ -30,11 +32,12 @@ return {
                     end
 
                     -- code search
-                    map("gd", builtin.lsp_definitions, { desc = "[G]oto [D]efinition" })
-                    map("<leader>gd", builtin.lsp_definitions, { desc = "[G]oto [D]efinition" })
+                    map("gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
+                    map("<leader>gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
                     map("<leader>gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" }) -- like headers in c/c++
-                    map("<leader>gr", builtin.lsp_references , { desc = "[G]oto [R]eferences" })
-                    map("<leader>gt", builtin.lsp_type_definitions , { desc = "[G]oto [T]ype definition" }) -- find type of symbol under cursor
+                    map("<leader>gr", builtin.lsp_references, { desc = "[G]oto [R]eferences" })
+                    map("<leader>gr", builtin.lsp_implementations, { desc = "[G]oto [I]mplementations" })
+                    map("<leader>gt", vim.lsp.buf.type_definition, { desc = "[G]oto [T]ype definition" }) -- find type of symbol under cursor
 
                     -- symbol manipulation
                     map("<leader>ca", vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
@@ -74,6 +77,16 @@ return {
                 },
             },
             signature = { enabled = true },
+
+            keymap = {
+                preset = 'default',
+
+                ['<C-a>'] = {'show', 'fallback'}, -- c-space cannot be called in powershell
+                ['<C-Space>'] = {'show', 'fallback'},
+                --['<Esc>'] = {'hide', 'fallback'},
+                ['<CR>'] = {'accept', 'fallback'},
+            },
+
         },
 
         opts_extend = { 'sources.default' },
