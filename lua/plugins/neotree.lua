dbb530eaa54dbdb6459209038643f350ce4e1ce4
Jordan Thomas <jordanjothomas@gmail.com>
2026-02-09 16:23:40 -0500

    Added s1n7ax/nvim-window-picker
    I got annoyed with neotree opening windows in places that I didnt like,
    so I added this plugin to fix that issue. Split your panels the way you
    want to use them, open neotree, hover over the file you want to open and
    press `w`. You will be prompted with keys on the screen for each window,
    to select where the file will be opened!

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,58 +1,68 @@
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-        'MunifTanjim/nui.nvim',
+    {
+        's1n7ax/nvim-window-picker',
+        name = 'window-picker',
+        event = 'VeryLazy',
+        version = '2.*',
+        opts = { hint = 'floating-letter'}
     },
-    cmd = 'Neotree',
-    keys = {
-        -- Open side tree
-        { '\\', function ()
-            --print(vim.bo.filetype)
-            if vim.bo.filetype == 'neo-tree' then
-                vim.cmd("Neotree close")
-            else
-                vim.cmd("Neotree reveal position=left")
-            end
-        end, desc = 'NeoTree reveal', silent = true },
 
-        -- Explore command
-        { '<leader>ex', ':Neotree current<CR>', desc = 'NeoTree reveal', silent = true },
-    },
-    opts = function(_, opts)
-        -- remove netrw and replace
-        opts.filesystem = opts.filesystem or {}
-        opts.filesystem.hijack_netrw_behavior =
-        opts.filesystem.hijack_netrw_behavior or "open_current"
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        version = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+            'MunifTanjim/nui.nvim',
+        },
+        cmd = 'Neotree',
+        keys = {
+            -- Open side tree
+            { '\\', function ()
+                --print(vim.bo.filetype)
+                if vim.bo.filetype == 'neo-tree' then
+                    vim.cmd("Neotree close")
+                else
+                    vim.cmd("Neotree reveal position=left")
+                end
+            end, desc = 'NeoTree reveal', silent = true },
 
-        opts.filesystem.window = opts.filesystem.window or {}
-        opts.filesystem.window.position =
-        opts.filesystem.window.position or "current"
+            -- Explore command
+            { '<leader>ex', ':Neotree current<CR>', desc = 'NeoTree reveal', silent = true },
+        },
+        opts = function(_, opts)
+            -- remove netrw and replace
+            opts.filesystem = opts.filesystem or {}
+            opts.filesystem.hijack_netrw_behavior =
+            opts.filesystem.hijack_netrw_behavior or "open_current"
 
-        opts.filesystem.window.mappings =
-        vim.tbl_extend("force", opts.filesystem.window.mappings or {}, {
-            ["\\"] = "close_window",
-        })
+            opts.filesystem.window = opts.filesystem.window or {}
+            opts.filesystem.window.position =
+            opts.filesystem.window.position or "current"
 
-        -- Integrate snack rename
-        local events = require("neo-tree.events")
+            opts.filesystem.window.mappings =
+            vim.tbl_extend("force", opts.filesystem.window.mappings or {}, {
+                ["\\"] = "close_window",
+            })
 
-        opts.event_handlers = opts.event_handlers or {}
-        vim.list_extend(opts.event_handlers, {
-            {
-                event = events.FILE_MOVED,
-                handler = function(data)
-                    Snacks.rename.on_rename_file(data.source, data.destination)
-                end,
-            },
-            {
-                event = events.FILE_RENAMED,
-                handler = function(data)
-                    Snacks.rename.on_rename_file(data.source, data.destination)
-                end,
-            },
-        })
-    end,
+            -- Integrate snack rename
+            local events = require("neo-tree.events")
+
+            opts.event_handlers = opts.event_handlers or {}
+            vim.list_extend(opts.event_handlers, {
+                {
+                    event = events.FILE_MOVED,
+                    handler = function(data)
+                        Snacks.rename.on_rename_file(data.source, data.destination)
+                    end,
+                },
+                {
+                    event = events.FILE_RENAMED,
+                    handler = function(data)
+                        Snacks.rename.on_rename_file(data.source, data.destination)
+                    end,
+                },
+            })
+        end,
+    }
 }
